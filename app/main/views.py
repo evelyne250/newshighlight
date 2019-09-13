486a23a4a14498858e6b21c728b804f23376bb44
pascaline <passyish5@gmail.com>
2019-09-13 11:26:55 +0200

    added a title

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,7 @@
 from flask import render_template
 from . import main
+from ..request import get_sources
+from ..models import Sources
 
 # Views
 @main.route('/')
@@ -8,4 +10,14 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    return render_template('index.html')
+    
+    new_general = get_sources('general')
+    new_business = get_sources('business')
+    new_entertainment = get_sources('entertainment')
+    new_sports = get_sources('sports')
+    new_tech = get_sources('technology')
+    new_science = get_sources('science')
+    new_health = get_sources('health')
+
+    title = 'Home | Best News Highlight'
+    return render_template('index.html',title = title)
